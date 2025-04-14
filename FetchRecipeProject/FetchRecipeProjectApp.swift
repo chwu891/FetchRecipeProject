17b90cf272f9061d9521cfad389409090e387aec
Chi-Hsien Wu <chwu891@gmail.com>
2025-04-14 15:44:58 -0700

    Initial Commit

---
+++ FetchRecipeProject/FetchRecipeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeProjectApp.swift
+//  FetchRecipeProject
+//
+//  Created by Chi-Hsien Wu on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipeProjectTests/FetchRecipeProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeProjectTests.swift
+//  FetchRecipeProjectTests
+//
+//  Created by Chi-Hsien Wu on 4/14/25.
+//
+
+import Testing
+@testable import FetchRecipeProject
+
+struct FetchRecipeProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
